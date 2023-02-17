3e6585f834f11317cb3c136f51a60bca24572e1b
m00p1ng <mongkonchai4412@gmail.com>
2023-02-17 20:29:35 +0700

    add component

--- 02-component/src/App.vue
+++ 02-component/src/App.vue
@@ -1,26 +1,57 @@
 <template>
   <div>
+    <!-- <the-header></the-header> -->
+    <!-- <badge-list></badge-list> -->
+    <!-- <user-info -->
+    <!--   :full-name="activeUser.name" -->
+    <!--   :info-text="activeUser.description" -->
+    <!--   :role="activeUser.role" -->
+    <!-- ></user-info> -->
+    <!-- <course-goals #default="slotProps"> -->
+    <!--   <h2>{{ slotProps.item }}</h2> -->
+    <!--   <p>{{ slotProps['another-props']}}</p> -->
+    <!-- </course-goals> -->
+
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+    <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script lang="ts">
+import TheHeader from './components/layout/TheHeader.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue'
+import ActiveGoals from './components/ActiveGoals.vue'
+import ManageGoals from './components/ManageGoals.vue'
+
 export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'mooping',
         description: 'Site owner and admin',
         role: 'admin'
       }
     }
-  }
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  },
 }
 </script>
 
---
+++ 02-component/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-props="..."></slot>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      goals: ['Finish the course', 'Learn Vue']
+    };
+  },
+}
+</script>
+
