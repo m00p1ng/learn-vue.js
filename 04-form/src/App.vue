eaa0f7ded0aa2281b8b7f9e6ea961fe2db4e2629
m00p1ng <mongkonchai4412@gmail.com>
2023-02-17 22:36:50 +0700

    add basic

--- 04-form/src/App.vue
+++ 04-form/src/App.vue
@@ -1,3 +1,28 @@
-<template></template>
+<template>
+  <the-form></the-form>
+</template>
 
-<style scoped></style>
+<script lang="ts">
+import TheForm from './components/TheForm.vue';
+
+export default {
+  components: {
+    TheForm
+  }  
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
