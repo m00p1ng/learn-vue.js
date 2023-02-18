4154e31109fe0d847600ee791a482dd90d056214
m00p1ng <mongkonchai4412@gmail.com>
2023-02-18 21:55:18 +0700

    init vuex

--- 08-vuex/src/App.vue
+++ 08-vuex/src/App.vue
@@ -1,3 +1,27 @@
-<template></template>
+<template>
+  <base-container title="Vuex"></base-container>
+</template>
 
-<style scoped></style>
+<script lang="ts">
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
