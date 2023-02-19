6ae8918663be120c8ea33389613805faa844d894
m00p1ng <mongkonchai4412@gmail.com>
2023-02-19 17:36:15 +0700

    add vuex

---
+++ 08-vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({value: 11})">Add 11</button>
+</template>
+
+<script lang="ts">
+import { mapActions } from 'vuex'
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.commit('increment')
+    // }
+    // ...mapActions(['increment', 'increase'])
+    ...mapActions('numbers', {
+      inc: 'increment',
+      increase: 'increase'
+    })
+  }
+}
+</script>
---
+++ 08-vuex/src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>{{ finalCounter }}</h3>
+</template>
+
+<script lang="ts">
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters('numbers', ['finalCounter']),
+  },
+}
+</script>
